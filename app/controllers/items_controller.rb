75ee53aa978fcba16ca3e6abdd922fa60ea909ad
kohei0429 <taratyaan@gmail.com>
2023-03-04 18:58:43 +0900

    Active Hash 各項目のモデル作成&紐付け

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,15 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :title, :text, :category_id, :condition_id, :delivery_charge_id, :shipping_area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :shipping_area
+  belongs_to :shipping_day
+
+  valitades :title, :text, :price, presence: true
+
+  validates :category_id, :condition_id, :delivery_charge_id, :shipping_area_id, :shipping_day_id,  numericality: { other_than: 1 , message: "can't be blank" } 
 end
