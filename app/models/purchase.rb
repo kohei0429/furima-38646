9dcf7e8bd8d470d1ce798d21587e26f8c56c9287
kohei0429 <taratyaan@gmail.com>
2023-03-14 20:08:31 +0900

    purchase, ship モデルの編集

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,9 @@
 class Purchase < ApplicationRecord
+
+  validates: item, presense: true
+  
+  belongs_to :user
+  belongs_to :item
+  has_one :ship
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,15 @@
 class Ship < ApplicationRecord
+
+  belongs_to :purchase
+
+  with_options presence: true do
+    validates :postcode
+    validates :shipping_area_id
+    validates :city
+    validates :city_number
+    validates :building_name
+    validates :phone_number
+    validates :purchase
+  end
+  
 end
