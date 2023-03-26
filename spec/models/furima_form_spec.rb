4843f25acc486d5932fabc054db0a2904a28ee01
kohei0429 <taratyaan@gmail.com>
2023-03-26 18:59:21 +0900

    user_id, item_idの記述を、factoryからspecへ変更

--- spec/models/furima_form_spec.rb
+++ spec/models/furima_form_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 RSpec.describe FurimaForm, type: :model do
 
   before do
-    @furima_form = FactoryBot.build(:furima_form)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @furima_form = FactoryBot.build(:furima_form, user_id: user.id, item_id: item.id)
   end
 
   describe '商品購入' do
